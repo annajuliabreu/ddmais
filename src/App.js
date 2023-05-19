5f6055782adee437a8b1b4cd7ccc8fcfa56d7b98
Anna Julia <74986458+annajuliabreu@users.noreply.github.com>
2023-05-19 07:12:02 -0300

    homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { PaginaPrincipal } from './components/pages/PaginaPrincipal/PaginaPrincipal';
 
-function App(){
-  return(
-    <div>
-     <PaginaPrincipal />
+function App() {
+  return (
+    <div className="App">
+        <PaginaPrincipal />
     </div>
-  )
+  );
 }
 
 export default App;
