0a2cebfefa5191f4aa3b6e2687c69fb79400ae62
Anna Julia <74986458+annajuliabreu@users.noreply.github.com>
2023-05-19 08:00:24 -0300

    homepage

--- src/components/commom/Galeria/Galeria.jsx
+++ src/components/commom/Galeria/Galeria.jsx
@@ -3,15 +3,15 @@ import "./Galeria.css"
 export function Galeria() {
     return (
         <div className="galeria">
-            <img className="img" src="../plantita.png" alt="" />
-            <img className="img" src="../ddmais.png" alt="" />
-            <img className="img" src="../pega.png" alt="" />
-            <img className="img" src="../adore.png" alt="" />
-            <img className="img" src="../aquiacola.png" alt="" />
-            <img className="img" src="../linkou.png" alt="" />
-            <img className="img" src="../disneyplus.png" alt="" />
-            <img className="img" src="../ariel.png" alt="" />
-            <img className="img" src="../mural.png" alt="" />
+            <img className="img" src="https://i.pinimg.com/474x/19/b2/f8/19b2f8c6d6f2fd1f30a6d95e90530945.jpg" alt="" />
+            <img className="img" src="https://i.pinimg.com/564x/2d/39/12/2d391261f1bde79cee30c7570daa283d.jpg" alt="" />
+            <img className="img" src="https://i.pinimg.com/474x/87/af/91/87af913639f05e5e74037450b8c1d5ed.jpg" alt="" />
+            <img className="img" src="https://i.pinimg.com/474x/c6/6d/41/c66d4165221abd4555a27ab76ba343a9.jpg" alt="" />
+            <img className="img" src="https://i.pinimg.com/564x/01/02/a1/0102a10107f3f20fb526a708f1e9b5bb.jpg" alt="" />
+            <img className="img" src="https://i.pinimg.com/474x/19/b2/f8/19b2f8c6d6f2fd1f30a6d95e90530945.jpg" alt="" />
+            <img className="img" src="https://i.pinimg.com/474x/5e/92/98/5e9298e138770877a29d069a13747436.jpg" alt="" />
+            <img className="img" src="https://i.pinimg.com/474x/70/e6/fd/70e6fddb1ed5cece8b7aa7efc94afc6f.jpg" alt="" />
+            <img className="img" src="https://i.pinimg.com/474x/85/62/31/856231ca7e5a389411ae0160ed74aa46.jpg" alt="" />
         </div>
     )
 }
--- src/components/commom/Navegador/Navegador.jsx
+++ src/components/commom/Navegador/Navegador.jsx
@@ -1,4 +1,5 @@
 import "./Navegador.css"
+import "./logo.svg"
 
 export function Navegador(){
     // let Botao = (props) => (<a className="link-navegador" href="https://instagram.com/ddmais.ufc">{props.linkTexto}</a>);
@@ -6,7 +7,7 @@ export function Navegador(){
     return(
         <nav className="nav">
             <div className="div-right">
-                <a href="https://instagram.com/ddmais.ufc"><img className="img" src="../logo.svg" alt="" /></a>
+                <a href="https://instagram.com/ddmais.ufc"><img className="img" src="https://raw.githubusercontent.com/annajuliabreu/ddmais/b82e1d124f0e3bbc205bb84a12b1b6d3342a9b91/public/logo.svg" alt="" /></a>
                 <a className="botao-home" href="https://instagram.com/ddmais.ufc">Home</a>
                 <a className="botao-explorar" href="https://instagram.com/ddmais.ufc">Explorar</a>
             </div>
