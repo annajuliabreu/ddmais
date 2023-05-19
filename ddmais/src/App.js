37b246f120e20da870ddcb56cf45f33cfaec299f
Anna Julia <74986458+annajuliabreu@users.noreply.github.com>
2023-05-18 23:06:48 -0300

    home

---
+++ ddmais/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { PaginaPrincipal } from './page/PaginaPrincipal/PaginaPrincipal';
+
+function App() {
+  return (
+    <div className="App">
+        <PaginaPrincipal />
+    </div>
+  );
+}
+
+export default App;
---
+++ ddmais/src/page/PaginaPrincipal/PaginaPrincipal.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import { Navegador } from "../../commom/Navegador/Navegador";
+import { Galeria } from "../../commom/Galeria/Galeria";
+
+export function PaginaPrincipal() {
+    return (
+        <Fragment>
+            <Navegador />
+            <Galeria />
+        </Fragment>
+    )
+}
